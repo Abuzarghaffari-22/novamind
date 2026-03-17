899e3c6ca648b0d91ff6d409f3f2c226d1590ebc
Abuzar Ghaffari <ag.businessofficial22@gmail.com>
2026-03-17 14:56:28 +0500

    Delete backend/nova directory

--- backend/nova/lib/python3.12/site-packages/smithy_http/exceptions.py
+++
@@ -1,7 +0,0 @@
-#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#  SPDX-License-Identifier: Apache-2.0
-from smithy_core.exceptions import SmithyError
-
-
-class SmithyHTTPError(SmithyError):
-    """Base exception type for all exceptions raised in HTTP clients."""
